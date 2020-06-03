68694c76f40b39cf1df2d26e072ec538fa8f533e
Abel Segui <Abelsegui@hotmail.com>
2020-06-03 03:33:21 +0000

    17_done

--- exercises/18-Create-a-for-loop/app.js
+++ exercises/18-Create-a-for-loop/app.js
@@ -1,7 +1,11 @@
 /* This exercise should be moved before 17-Your first loop? */
 
 // Declare and write your function here:
-
+function standardsMaker(){
+    for (i=0;i<300;i++)
+    {}
+    return i
+}
 
 // Calling the function:
 standardsMaker();
